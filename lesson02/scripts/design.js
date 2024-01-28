39513aa5762b190e169f5fb7ad668bec654b55ac
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-01-28 03:01:45 +0100

    Finished working on mobile view

--- lesson02/scripts/design.js
+++ lesson02/scripts/design.js
@@ -1,5 +1,14 @@
+const options = {
+    weekday : 'long',
+    day : 'numeric',
+    month : 'long',
+    year : 'numeric'
+}
 function getCurrentYear() {
     return new Date().getFullYear();
+}
+function getModifiedDate() {
+    return new Date(document.lastModified).toLocaleDateString('en-US', options);
 }
 function insertDateAndIcon() {
     const copyrightAndYear = document.querySelector(".copy");
@@ -14,5 +23,24 @@ function insertDateAndIcon() {
     copyrightAndYear.appendChild(copyrightSymbol);
     copyrightAndYear.appendChild(dateTextNode);
 }
+function insertTextAndModifiedDate() {
+    const dateContainer = document.getElementById('lastModified');
+
+    // Create an icon element (Font Awesome icon used as an example)
+    // const iconElement = document.createElement('i');
+    // iconElement.classList.add('icon', 'fas', 'fa-calendar');
+
+    // Create a text node with Last Modified Date
+    const lastModifiedDate = document.createTextNode("Last Modified Date: ");
+
+    // Create a text node with the current date
+    const dateTextNode = document.createTextNode(getModifiedDate());
+
+    // Append the icon and text node to the container element
+    dateContainer.appendChild(lastModifiedDate);
+    dateContainer.appendChild(dateTextNode);
+}
 // Call the function to insert the date and icon when the page is loaded
 document.addEventListener('DOMContentLoaded', insertDateAndIcon);
+// Call the function to insert the date and text when the page is loaded
+document.addEventListener('DOMContentLoaded', insertTextAndModifiedDate);
