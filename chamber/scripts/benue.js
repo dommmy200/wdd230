121fc9502ab2e31b6b2c2a97c427d0cd321cc2a6
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-02-04 05:58:46 +0100

    One Hour to Submission

--- chamber/scripts/benue.js
+++ chamber/scripts/benue.js
@@ -13,7 +13,7 @@ function getModifiedDate() {
 }
 
 function insertDateAndIcon() {
-    const dateContainer = document.querySelector(".copyright-logo");
+    const dateContainer = document.querySelector("#copy-date");
 
     // Create a text node with the copyright symbol
     const copyrightSymbol = document.createTextNode(' \u00A9'); // Unicode character for the copyright symbol
@@ -43,30 +43,6 @@ function insertTextAndModifiedDate() {
     dateContainer.appendChild(lastModifiedDate);
     dateContainer.appendChild(dateTextNode);
 }
-// Create a new Image object
-var image = new Image();
-// Set the source (URL) of the image
-image.src = '/lesson03/images/id-city.jpeg';
-
-image.onload = function() {
-    // Get the placeholder element by its ID
-    var imageElement = document.getElementById('dynamicImage');
-
-    // Update the src attribute of the image element with the loaded image
-    // imageElement.src = image.src;
-    
-    // You can also set other attributes or styles if needed
-    imageElement.alt = 'Main image of Edumoga Chamber of Commerce';
-    imageElement.style.width = '100%';
-    // imageElement.style.height = 'auto';
-    imageElement.style.height = "465px";    
-    // Set the background image of the container
-    imageElement.style.backgroundImage = 'url(' + image.src + ')';
-};
-
-// Append the image object to the body (this triggers the image loading)
-document.body.main.appendChild(image);
-
 // Call the function to insert the date and icon when the page is loaded
 document.addEventListener('DOMContentLoaded', insertDateAndIcon);
 // Call the function to insert the date and text when the page is loaded
