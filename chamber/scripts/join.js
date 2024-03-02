8b8ce53656754ed9aee7d52c705bd7c4d3b414fb
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-03-02 05:53:28 +0100

    Css validation

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,4 +1,4 @@
-const submitTime = document.getElementById('submit-time');
+// const submitTime = document.getElementById('submit-time');
 const submitDate = document.getElementById('submit-date');
 
 const form = document.querySelector('#join-form');
@@ -6,9 +6,9 @@ const form = document.querySelector('#join-form');
 const dateTime = new Date();
 
 const currentDate = dateTime.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'});
-const currentTime = dateTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', second:'2-digit'});
+// const currentTime = dateTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', second:'2-digit'});
 
 form.addEventListener('submit', () => {
-    submitTime.value = currentTime;
+    // submitTime.value = currentTime;
     submitDate.value = currentDate;
 });
